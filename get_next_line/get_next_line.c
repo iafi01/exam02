5bfed3515bb514d6753f75d2c6dbc27db04e19e4
IAFI <67833440+iafi01@users.noreply.github.com>
2021-02-21 14:11:15 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,19 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/19 17:14:15 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/21 09:24:16 by liafigli         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdlib.h>
-
-int ft_strlen(char *s)
+static int ft_strlen(char *s)
 {
     int i;
 
@@ -23,7 +8,7 @@ int ft_strlen(char *s)
     return (i);
 }
 
-char *ft_memcpy(char *dst, char *src, int n)
+static char *ft_memcpy(char *dst, char *src, int n)
 {
      int i = 0;
      while (i < n)
@@ -54,8 +39,14 @@ int get_next_line(char **line)
     }
     buf[i] = 0;
     if (!(str = (char *)malloc(ft_strlen(buf) + 1)))
-        str = NULL;
+        str = 0;
     ft_memcpy(str, buf, ft_strlen(buf) + 1);
     *line = str;
     return (o);
 }
+/*#include <stdio.h>
+int main()
+{
+    char **line = NULL;
+    printf("%d",get_next_line(line));
+}*/
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/19 17:15:43 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/19 17:22:14 by liafigli         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+static int ft_strlen(char *s)
+{
+    int i;
 
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+static char *ft_memcpy(char *dst, char *src, int n)
+{
+     int i = 0;
+     while (i < n)
+     {
+         dst[i] = src[i];
+         i++;
+     }
+     dst[i] = 0;
+     return (dst);
+}
+
+int get_next_line(char **line)
+{
+    char c;
+    char *str;
+    int i = 0;
+    int o = 0;
+    char buf[99999];
+
+    while (i < 99999)
+        buf[i++] = 0;
+    i = 0;
+    while ((o = read(0, &c, 1)) > 1)
+    {
+        if (c == '\n')
+            break;
+        buf[i++] = c;
+    }
+    buf[i] = 0;
+    if (!(str = (char *)malloc(ft_strlen(buf) + 1)))
+        str = 0;
+    ft_memcpy(str, buf, ft_strlen(buf) + 1);
+    *line = str;
+    return (o);
+}
+/*#include <stdio.h>
+int main()
+{
+    char **line = NULL;
+    printf("%d",get_next_line(line));
+}*/
